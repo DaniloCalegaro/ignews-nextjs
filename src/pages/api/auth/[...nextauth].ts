493ab0256eb31079b78faef0a02826a3c1957cd6
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-07-05 16:28:16 -0300

    feat: implementando função subscribe

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,15 +20,35 @@ export default NextAuth({
   callbacks: {
     async signIn({ user, account, profile }) {
       const { email } = user
-      //console.log(email)
-      // await fauna.query(
-      //   q.Create(
-      //     q.Collection('users'),
-      //     { data: { email } }
-      //   )
-      // )
 
-      return true
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email as string)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email as string)
+              )
+            )
+          )
+        )
+        return true
+
+      } catch {
+        return false
+      }
     },
   }
 })
